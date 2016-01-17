46d8b3abe204b4c6f07d2657b23777b81e062eab
Tanguy Krotoff <tkrotoff@gmail.com>
2016-01-17 16:44:19 +0100

    Force JavaScript language + results.json

--- src/Boss.js
+++ src/Boss.js
@@ -1,8 +1,8 @@
 import {File} from './Database';
 import Analyser from './Analyser';
 
-let language = 'language';
-let db = new File('File path and name where to save the results');
+let language = 'javascript';
+let db = new File('results.json'); // File path and name where to save the results
 let githubToken = 'Your Personal Github TOKEN (only public_repo rights needed)';
 
 setInterval(function () {
